5dcb50c3658d9231a0f17612be75ac41dd5b8991
Ragib <ragibhasan894@gmail.com>
2020-04-18 18:51:26 +0600

    installed and configured nodemon, morgan; handling errors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 // products
 const productRoutes = require('./api/routes/products');
@@ -11,7 +12,27 @@ const orderRoutes = require('./api/routes/orders');
     });
 });*/
 
+app.use(morgan('dev'));
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// Error Handling
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+});
+
+// Error from anywhere in this app
+app.use((error,req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 
 module.exports = app;
