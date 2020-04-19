dadc8e953d997ab11db91129802069ffb1c06d43
Ragib <ragibhasan894@gmail.com>
2020-04-20 00:46:32 +0600

    installed mongoose, connected mongoDB atlas, saving new product

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 // Get all prodcuts
 router.get('/', (req, res, next) => {
@@ -10,20 +13,54 @@ router.get('/', (req, res, next) => {
 
 // Create a new product
 router.post('/', (req, res, next) => {
-    const product = {
+    /*const product = {
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(200).json({
-        message: "Handling POST requests to /procucts",
-        createdProduct: product
+    };*/
+
+    // creating instance of Product
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price,
     });
+    // save product
+    product.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: "Handling POST requests to /procucts",
+            createdProduct: result
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+
+    
 });
 
 // View a product info
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if (id === 'special') {
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log("From DB", doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({message: "No record found!"});
+            }
+            
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
+        });
+    /*if (id === 'special') {
         res.status(200).json({
             message: "You have got a special ID!"
         });
@@ -31,7 +68,7 @@ router.get('/:productId', (req, res, next) => {
         res.status(200).json({
             message: `You have passed id: ${id}`
         });
-    }    
+    }*/    
 });
 
 // Update product
--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // products
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://ragib-hasan:'+ process.env.MONGO_ATLAS_PW +'@cluster0-esroh.mongodb.net/test?retryWrites=true&w=majority', 
+{
+    useMongoClient: true
+}
+);
 
 /*app.use((req, res, next) => {
     res.status(200).json({
